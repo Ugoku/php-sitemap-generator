9d931580640ceb8468e642ae60c6821d991e3aa3
Sander <bitbucket@ugoku.nl>
2018-08-30 12:54:32 +0200

    Code style

--- SitemapGenerator.php
+++ SitemapGenerator.php
@@ -3,9 +3,9 @@
  * @category   Class
  * @package    SitemapGenerator
  * @author     Paweł Antczak <pawel@antczak.org>, Sander de Jong <github@ugoku.nl>
- * @copyright  2009 Paweł Antczak, 2016 Ugoku
+ * @copyright  2009 Paweł Antczak, 2016-2018 Ugoku
  * @license    https://www.gnu.org/licenses/gpl.html  GPL V 2.0
- * @version    2.0.0
+ * @version    2.0.1
  * @see        http://www.sitemaps.org/protocol.php
  * @see        https://en.wikipedia.org/wiki/Site_map
  * @see        https://en.wikipedia.org/wiki/Sitemaps
@@ -19,20 +19,20 @@ class SitemapGenerator
      * @var string
      * @access public
      */
-    public $sitemapFileName = "sitemap.xml";
+    public $sitemapFileName = 'sitemap.xml';
     /**
      * Name of sitemap index file
      * @var string
      * @access public
      */
 
-    public $sitemapIndexFileName = "sitemap-index.xml";
+    public $sitemapIndexFileName = 'sitemap-index.xml';
     /**
      * Robots file name
      * @var string
      * @access public
      */
-    public $robotsFileName = "robots.txt";
+    public $robotsFileName = 'robots.txt';
     /**
      * Quantity of URLs per single sitemap file.
      * According to specification max value is 50.000.
@@ -71,18 +71,20 @@ class SitemapGenerator
      * @var string
      * @access private
      */
-    private $classVersion = "1.2.0";
+    private $classVersion = '1.2.1';
     /**
      * Search engines URLs
      * @var array of strings
      * @access private
      */
     private $searchEngines = [
-        ["http://search.yahooapis.com/SiteExplorerService/V1/updateNotification?appid=USERID&url=",
-        "http://search.yahooapis.com/SiteExplorerService/V1/ping?sitemap="],
-        "https://www.google.com/webmasters/tools/ping?sitemap=",
-        "http://submissions.ask.com/ping?sitemap=",
-        "https://www.bing.com/webmaster/ping.aspx?siteMap="
+        [
+            'http://search.yahooapis.com/SiteExplorerService/V1/updateNotification?appid=USERID&url=',
+            'http://search.yahooapis.com/SiteExplorerService/V1/ping?sitemap='
+        ],
+        'https://www.google.com/webmasters/tools/ping?sitemap=',
+        'http://submissions.ask.com/ping?sitemap=',
+        'https://www.bing.com/webmaster/ping.aspx?siteMap='
     ];
     /**
      * Array with urls
@@ -104,7 +106,7 @@ class SitemapGenerator
      */
 
     private $sitemapIndex;
-     /**
+    /**
      * Current sitemap full URL
      * @var string
      * @access private
@@ -116,7 +118,7 @@ class SitemapGenerator
      * @param string $baseURL You site URL, with / at the end.
      * @param string|null $basePath Relative path where sitemap and robots should be stored.
      */
-    public function __construct($baseURL, $basePath = "")
+    public function __construct(string $baseURL, $basePath = null)
     {
         $this->baseURL = $baseURL;
         $this->basePath = $basePath;
@@ -130,17 +132,17 @@ class SitemapGenerator
      *
      * @throws \InvalidArgumentException
      */
-    public function addUrls($urlsArray)
+    public function addUrls(array $urlsArray)
     {
         if (!is_array($urlsArray)) {
-            throw new \InvalidArgumentException("Array as argument should be given.");
+            throw new \InvalidArgumentException('Array as argument should be given.');
         }
         foreach ($urlsArray as $url) {
             $this->addUrl(
-                isset($url[0]) ? $url[0] : null,
-                isset($url[1]) ? $url[1] : null,
-                isset($url[2]) ? $url[2] : null,
-                isset($url[3]) ? $url[3] : null
+                $url[0] ?? null,
+                $url[1] ?? null,
+                $url[2] ?? null,
+                $url[3] ?? null
             );
         }
     }
@@ -158,10 +160,10 @@ class SitemapGenerator
      * @see https://en.wikipedia.org/wiki/ISO_8601
      * @see https://secure.php.net/manual/en/function.date.php
      */
-    public function addUrl($url, $lastModified = null, $changeFrequency = null, $priority = null)
+    public function addUrl(string $url, string $lastModified = null, string $changeFrequency = null, string $priority = null)
     {
         if ($url == null) {
-            throw new \InvalidArgumentException("URL is mandatory. At least one argument should be given.");
+            throw new \InvalidArgumentException('URL is mandatory. At least one argument should be given.');
         }
         $urlLength = extension_loaded('mbstring') ? mb_strlen($url) : strlen($url);
         if ($urlLength > 2048) {
@@ -187,30 +189,30 @@ class SitemapGenerator
     public function createSitemap()
     {
         if (!isset($this->urls)) {
-            throw new \BadMethodCallException("To create sitemap, call addUrl or addUrls function first.");
+            throw new \BadMethodCallException('To create sitemap, call addUrl or addUrls function first.');
         }
         if ($this->maxURLsPerSitemap > 50000) {
-            throw new \InvalidArgumentException("More than 50,000 URLs per single sitemap is not allowed.");
+            throw new \InvalidArgumentException('More than 50,000 URLs per single sitemap is not allowed.');
         }
 
-        $generatorInfo = '<!-- generator="SimpleSitemapGenerator/'.$this->classVersion.'" -->
-                          <!-- sitemap-generator-url="https://github.com/Ugoku/php-sitemap-generator"
-                          sitemap-generator-version="'.$this->classVersion.'" -->
-                          <!-- generated-on="'.date('c').'" -->';
-        $sitemapHeader = '<?xml version="1.0" encoding="UTF-8"?>'.$generatorInfo.'
-                            <urlset
-                                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                                xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-                                xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                         </urlset>';
-        $sitemapIndexHeader = '<?xml version="1.0" encoding="UTF-8"?>'.$generatorInfo.'
-                                <sitemapindex
-                                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                                    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-                                    http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
-                                    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-                              </sitemapindex>';
+        $generatorInfo = '<!-- generator="SimpleSitemapGenerator/' . $this->classVersion . '" -->
+            <!-- sitemap-generator-url="https://github.com/Ugoku/php-sitemap-generator"
+            sitemap-generator-version="' . $this->classVersion . '" -->
+            <!-- generated-on="' . date('c') . '" -->';
+        $sitemapHeader = '<?xml version="1.0" encoding="UTF-8"?>' . $generatorInfo . '
+            <urlset
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+                xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+         </urlset>';
+        $sitemapIndexHeader = '<?xml version="1.0" encoding="UTF-8"?>' . $generatorInfo . '
+            <sitemapindex
+                xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd"
+                xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+          </sitemapindex>';
         foreach (array_chunk($this->urls, $this->maxURLsPerSitemap) as $sitemap) {
             $xml = new \SimpleXMLElement($sitemapHeader);
             foreach ($sitemap as $url) {
@@ -227,18 +229,18 @@ class SitemapGenerator
                 }
             }
             if (strlen($xml->asXML()) > 10485760) {
-                throw new \LengthException("Sitemap size is more than 10MB (10,485,760), please decrease maxURLsPerSitemap variable.");
+                throw new \LengthException('Sitemap size is more than 10MB (10,485,760), please decrease maxURLsPerSitemap variable.');
             }
             $this->sitemaps[] = $xml->asXML();
         }
 
-        if (sizeof($this->sitemaps) > 1000) {
+        if (count($this->sitemaps) > 1000) {
             throw new \LengthException("Sitemap index contains 1000 single sitemaps. Perhaps you're trying to submit too many URLs.");
         }
-        if (sizeof($this->sitemaps) > 1) {
-            for ($i = 0; $i < sizeof($this->sitemaps); $i++) {
+        if (count($this->sitemaps) > 1) {
+            for ($i = 0; $i < count($this->sitemaps); $i++) {
                 $this->sitemaps[$i] = [
-                    str_replace(".xml", ($i+1).".xml.gz", $this->sitemapFileName),
+                    str_replace('.xml', ($i + 1) . '.xml.gz', $this->sitemapFileName),
                     $this->sitemaps[$i]
                 ];
             }
@@ -248,13 +250,14 @@ class SitemapGenerator
                 $row->addChild('loc', $this->baseURL . htmlentities($sitemap[0]));
                 $row->addChild('lastmod', date('c'));
             }
-            $this->sitemapFullURL = $this->baseURL.$this->sitemapIndexFileName;
+            $this->sitemapFullURL = $this->baseURL . $this->sitemapIndexFileName;
             $this->sitemapIndex = [
                 $this->sitemapIndexFileName,
-                $xml->asXML()];
+                $xml->asXML()
+            ];
         } else {
             if ($this->createGZipFile) {
-                $this->sitemapFullURL = $this->baseURL . $this->sitemapFileName . ".gz";
+                $this->sitemapFullURL = $this->baseURL . $this->sitemapFileName . '.gz';
             } else {
                 $this->sitemapFullURL = $this->baseURL . $this->sitemapFileName;
             }
@@ -273,7 +276,7 @@ class SitemapGenerator
      *
      * @access public
      */
-    public function toArray()
+    public function toArray(): array
     {
         if (isset($this->sitemapIndex)) {
             return array_merge([$this->sitemapIndex], $this->sitemaps);
@@ -290,7 +293,7 @@ class SitemapGenerator
     public function writeSitemap()
     {
         if (!isset($this->sitemaps)) {
-            throw new \BadMethodCallException("To write sitemap, call createSitemap function first.");
+            throw new \BadMethodCallException('To write sitemap, call createSitemap function first.');
         }
         if (isset($this->sitemapIndex)) {
             $this->writeFile($this->sitemapIndex[1], $this->basePath, $this->sitemapIndex[0]);
@@ -300,7 +303,7 @@ class SitemapGenerator
         } else {
             $this->writeFile($this->sitemaps[0][1], $this->basePath, $this->sitemaps[0][0]);
             if ($this->createGZipFile) {
-                $this->writeGZipFile($this->sitemaps[0][1], $this->basePath, $this->sitemaps[0][0] . ".gz");
+                $this->writeGZipFile($this->sitemaps[0][1], $this->basePath, $this->sitemaps[0][0] . '.gz');
             }
         }
     }
@@ -312,30 +315,30 @@ class SitemapGenerator
     public function updateRobots()
     {
         if (!isset($this->sitemaps)) {
-            throw new \BadMethodCallException("To update robots.txt, call createSitemap function first.");
+            throw new \BadMethodCallException('To update robots.txt, call createSitemap function first.');
         }
         $sampleRobotsFile = "User-agent: *\nAllow: /";
-        if (file_exists($this->basePath.$this->robotsFileName)) {
-            $robotsFile = explode("\n", file_get_contents($this->basePath.$this->robotsFileName));
-            $robotsFileContent = "";
+        if (file_exists($this->basePath . $this->robotsFileName)) {
+            $robotsFile = explode("\n", file_get_contents($this->basePath . $this->robotsFileName));
+            $robotsFileContent = '';
             foreach ($robotsFile as $key => $value) {
-                if (substr($value, 0, 8) == 'Sitemap:') {
+                if (substr($value, 0, 8) === 'Sitemap:') {
                     unset($robotsFile[$key]);
                 } else {
-                    $robotsFileContent .= $value."\n";
+                    $robotsFileContent .= $value . "\n";
                 }
             }
             $robotsFileContent .= "Sitemap: $this->sitemapFullURL";
             if ($this->createGZipFile && !isset($this->sitemapIndex)) {
-                $robotsFileContent .= "\nSitemap: " . $this->sitemapFullURL . ".gz";
+                $robotsFileContent .= "\nSitemap: " . $this->sitemapFullURL . '.gz';
             }
             file_put_contents($this->basePath . $this->robotsFileName, $robotsFileContent);
         } else {
-            $sampleRobotsFile = $sampleRobotsFile."\n\nSitemap: ".$this->sitemapFullURL;
+            $sampleRobotsFile = $sampleRobotsFile."\n\nSitemap: " . $this->sitemapFullURL;
             if ($this->createGZipFile && !isset($this->sitemapIndex)) {
-                $sampleRobotsFile .= "\nSitemap: " . $this->sitemapFullURL . ".gz";
+                $sampleRobotsFile .= "\nSitemap: " . $this->sitemapFullURL . '.gz';
             }
-            file_put_contents($this->basePath.$this->robotsFileName, $sampleRobotsFile);
+            file_put_contents($this->basePath . $this->robotsFileName, $sampleRobotsFile);
         }
     }
 
@@ -352,28 +355,29 @@ class SitemapGenerator
      *
      * @access public
      */
-    public function submitSitemap($yahooAppId = null)
+    public function submitSitemap(string $yahooAppId = null): array
     {
         if (!isset($this->sitemaps)) {
-            throw new \BadMethodCallException("To submit sitemap, call createSitemap function first.");
+            throw new \BadMethodCallException('To submit sitemap, call createSitemap function first.');
         }
         if (!extension_loaded('curl')) {
-            throw new \BadMethodCallException("cURL library is needed to do submission.");
+            throw new \BadMethodCallException('cURL library is needed to do submission.');
         }
         $searchEngines = $this->searchEngines;
-        $searchEngines[0] = isset($yahooAppId) ? str_replace("USERID", $yahooAppId, $searchEngines[0][0]) : $searchEngines[0][1];
+        $searchEngines[0] = isset($yahooAppId) ? str_replace('USERID', $yahooAppId, $searchEngines[0][0]) : $searchEngines[0][1];
         $result = [];
-        for ($i = 0; $i < sizeof($searchEngines); $i++) {
+        for ($i = 0; $i < count($searchEngines); $i++) {
             $submitSite = curl_init($searchEngines[$i].htmlspecialchars($this->sitemapFullURL, ENT_QUOTES, 'UTF-8'));
             curl_setopt($submitSite, CURLOPT_RETURNTRANSFER, true);
             $responseContent = curl_exec($submitSite);
             $response = curl_getinfo($submitSite);
-            $submitSiteShort = array_reverse(explode(".", parse_url($searchEngines[$i], PHP_URL_HOST)));
+            $submitSiteShort = array_reverse(explode('.', parse_url($searchEngines[$i], PHP_URL_HOST)));
             $result[] = [
-                "site"=>$submitSiteShort[1].".".$submitSiteShort[0],
-                "fullsite"=>$searchEngines[$i].htmlspecialchars($this->sitemapFullURL, ENT_QUOTES, 'UTF-8'),
-                "http_code"=>$response['http_code'],
-                "message"=>str_replace("\n", " ", strip_tags($responseContent))];
+                'site' => $submitSiteShort[1] . '.' . $submitSiteShort[0],
+                'fullsite' => $searchEngines[$i] . htmlspecialchars($this->sitemapFullURL, ENT_QUOTES, 'UTF-8'),
+                'http_code' => $response['http_code'],
+                'message' => str_replace("\n", ' ', strip_tags($responseContent))
+            ];
         }
         return $result;
     }
@@ -388,9 +392,9 @@ class SitemapGenerator
      *
      * @access private
      */
-    private function writeFile($content, $filePath, $fileName)
+    private function writeFile(string $content, string $filePath, string $fileName): bool
     {
-        $file = fopen($filePath.$fileName, 'w');
+        $file = fopen($filePath . $fileName, 'w');
         fwrite($file, $content);
         return fclose($file);
     }
@@ -406,9 +410,9 @@ class SitemapGenerator
      *
      * @access private
      */
-    private function writeGZipFile($content, $filePath, $fileName)
+    private function writeGZipFile(string $content, string $filePath, string $fileName): bool
     {
-        $file = gzopen($filePath.$fileName, 'w');
+        $file = gzopen($filePath . $fileName, 'w');
         gzwrite($file, $content);
         return gzclose($file);
     }
